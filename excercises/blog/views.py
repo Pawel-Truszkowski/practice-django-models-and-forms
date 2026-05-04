270b0d97870283811fd498ca20d2c920557c180f
Pawel-Truszkowski <pawel.truszkowski14@gmail.com>
2026-05-04 21:06:31 +0200

    Completed task 03

--- excercises/blog/views.py
+++ excercises/blog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .forms import ContactForm
 
-# Create your views here.
+
+def contact_view(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            message = form.cleaned_data['message']
+            print(f"Received contact form submission: Email: {email}, Message: {message}")
+            return render(request, 'blog/contact.html', context={'form': ContactForm(), 'success': True})
+        else:
+            return render(request, 'blog/contact.html', context={'form': form, 'success': False})
+    else:
+        form = ContactForm()
+        return render(request, 'blog/contact.html', context={'form': form})
--- excercises/excercises/urls.py
+++ excercises/excercises/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from blog.views import contact_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('contact/', contact_view, name='contact'),
 ]
