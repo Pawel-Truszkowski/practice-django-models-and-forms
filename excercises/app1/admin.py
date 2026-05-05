a62fb014de324a18695e34a2a6e777b2c2a1bb1c
Pawel-Truszkowski <pawel.truszkowski14@gmail.com>
2026-05-05 21:39:25 +0200

    Completed task 05

--- excercises/app1/admin.py
+++ excercises/app1/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.db.models import F
 
-# Register your models here.
+from .models import Product
+
+
+@admin.action(description= "Apply discount")
+def discount_10_percent(modeladmin, request, queryset):
+    queryset.update(price= F('price') * 0.9)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'in_stock']
+
+    actions = [discount_10_percent]
+
+
+admin.site.register(Product, ProductAdmin)
